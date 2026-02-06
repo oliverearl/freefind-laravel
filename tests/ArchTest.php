536bd2201bda0305caa64e0bc8bbcf678b7d9c80
Oliver Earl <oliver@oliverearl.co.uk>
2026-02-06 07:33:40 +0000

    test: Use appropriate arch presets
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
+arch()->preset()->laravel();
+arch()->preset()->security();
+
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
