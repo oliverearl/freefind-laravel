f5124eedcfc4cf86411088f33d570022784bc689
Oliver Earl <oliver@oliverearl.co.uk>
2026-02-06 07:55:52 +0000

    test: configure test params
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace Freefind\Freefind\Tests;
 
+use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Freefind\Freefind\FreefindServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
+    use WithFaker;
+
     /** @inheritDoc */
     public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        config()->set('freefind-laravel.site_id', $this->faker->randomNumber(7));
     }
 
     /** @inheritDoc */
