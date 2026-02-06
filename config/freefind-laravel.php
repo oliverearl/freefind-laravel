794257d726a83d0a9db6f8a56f5b9d09492f0171
Oliver Earl <oliver@oliverearl.co.uk>
2026-02-06 07:34:44 +0000

    feat: config file
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- config/freefind-laravel.php
+++ config/freefind-laravel.php
@@ -1,6 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 // config for Freefind/Freefind
 return [
-
+    /*
+     * Your Freefind site ID. Without this, the package won't work properly. You can find this in your account settings
+     * in the top left hand corner of the Freefind control center. It is a numerical string.
+     */
+    'site_id' => (int) env('FREEFIND_SITE_ID', 0),
 ];
